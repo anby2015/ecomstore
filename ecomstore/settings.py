f46ea5f89971267b2be1f014113706dba7151650
ukolka <kolkabes@gmail.com>
2013-03-16 14:54:17 -0400

    added flatpages

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -102,6 +102,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'ecomstore.urls'
@@ -130,7 +131,8 @@ INSTALLED_APPS = (
     'ecomstore',
     'catalog',
     'utils',
-    'cart'
+    'cart',
+    'django.contrib.flatpages',
 )
 
 # A sample logging configuration. The only tangible logging
